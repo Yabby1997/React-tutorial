e1a0f22a2dc841c4a9c4a3140e6f95179750202e
yabby <yabby@Seunghun-MacBookPro.local>
2020-12-30 20:13:29 +0900

    React - 20.1. update 구현

--- src/components/UpdateContent.js
+++ src/components/UpdateContent.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 
-class CreateContent extends Component {
+class UpdateContent extends Component {
     render() {
         return (
             <article>
-                <h2>Create</h2>
-                <form action="/create_process" method="post"
+                <h2>Update</h2>
+                <form action="/update_process" method="post"
                     onSubmit={function(e){
                         e.preventDefault();
                         this.props.onSubmit(e.target.title.value, e.target.desc.value)
@@ -21,4 +21,4 @@ class CreateContent extends Component {
     }
 }
 
-export default CreateContent;
+export default UpdateContent;
